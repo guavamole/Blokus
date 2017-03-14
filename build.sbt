190d3888f515b6738ae97b47a6762c3664e47193
g3orgia <georgia.hong.1997@gmail.com>
2017-03-13 20:13:49 -0400

    initial project setup

---
+++ build.sbt
@@ -0,0 +1,6 @@
+name := "blokeus"
+
+version := "1.0"
+
+scalaVersion := "2.12.1"
+    
---
+++ src/main/scala-2.12/Game.scala
@@ -0,0 +1,8 @@
+/**
+  * Created by Georgia on 2017-03-13.
+  */
+object Game {
+  def main(args : Array[String]): Unit = {
+    println("BLOCKUSSS")
+  }
+}
